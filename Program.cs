720599a708cc773d2becb0b5691aee9c9f27305b
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-07-31 15:41:57 +0530

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace SnakeandLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("This problem simulates a Snake and Ladder Game");
+            int StartPositon = StartPosition.StartPositionInitialisation();
+            Console.WriteLine("Start Position " + StartPositon);
         }
     }
 }
